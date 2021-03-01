4153d563391ac96a4dfdfbd6e462ce7ea66c5360
Juan Rivera <juanrivera.primary@gmail.com>
2021-03-01 12:50:55 -0500

    added ds functionality

--- suggestify/src/components/dashboard/search/SearchBar.js
+++ suggestify/src/components/dashboard/search/SearchBar.js
@@ -1,39 +1,5 @@
 import React, { useState } from "react";
-
-// dummy data to test
-const dummyData = [
-	{
-		album_art: "https://i.scdn.co/image/ab67616d0000b273441fd03f69579d36801631d9",
-		album_name: "Houses of the Holy (1994 Remaster)",
-		artist: "Led Zeppelin",
-		title: "The Ocean - 1990 Remaster"
-	},
-	{
-		album_art: "https://i.scdn.co/image/ab67616d0000b273bfe41664b2ca1038b5e3bc6c",
-		album_name: "I Disappear",
-		artist: "Metallica",
-		title: "I Disappear"
-		},
-	{
-		album_art: "https://i.scdn.co/image/ab67616d0000b273533fd0b248052d04e6b732c0",
-		album_name: "Ride The Lightning (Deluxe Remaster)",
-		artist: "Metallica",
-		title: "Creeping Death (Remastered)"
-	},
-	{
-		album_art: "https://i.scdn.co/image/ab67616d0000b273eb11e2abccdca41f39ad3b89",
-		album_name: "Too Low For Zero",
-		artist: "Elton John",
-		title: "I Guess That's Why They Call It The Blues"
-	},
-	{
-		album_art: "https://i.scdn.co/image/ab67616d0000b273c199494ba9ea2b73e9208f91",
-		album_name: "Master of Reality (2014 Remaster)",
-		artist: "Black Sabbath",
-		title: "Sweet Leaf - 2014 Remaster"
-	}
-
-]
+import axios from 'axios';
 
 const initialSearch = {
 	artist: "",
@@ -54,7 +20,10 @@ const SearchBar = (props) => {
 	const handleSubmit = e => {
 		// use redux dispatches here
 		e.preventDefault()
-		props.handleSearch(dummyData)
+		axios.post(`https://spotify-3-ds.herokuapp.com/search/${search.artist}/${search.song}`)
+			.then(res => {
+				props.handleSearch(res.data)
+			})
 	};
 	return (
 		<div>
